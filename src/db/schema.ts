4353921c804ef3fbcd3247fcdd89c06cfeb7ec18
Jowi <83415433+JowiAoun@users.noreply.github.com>
2024-06-03 15:25:02 -0400

    Implement Mantine

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import {pgTable, serial} from "drizzle-orm/pg-core";
 
-export const bids = pgTable("biddy_bids", {
+export const bidsSchema = pgTable("biddy_bids", {
   id: serial("id").primaryKey(),
 })
